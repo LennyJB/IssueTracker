b6cc6d918a3bc66c5e6a6f3f0b7a0132238c394a
LennyJB <lennyjb@outlook.fr>
2023-11-16 08:43:18 +0100

    change setupBDD

--- src/main/java/geiffel/da4/issuetracker/IssueTrackerStudentsApplication.java
+++ src/main/java/geiffel/da4/issuetracker/IssueTrackerStudentsApplication.java
@@ -36,35 +36,41 @@ public class IssueTrackerStudentsApplication {
 
     @Bean
     public CommandLineRunner setUpBDD() {
-        return (args) -> {
+        return args -> {
+
+            List<User> users = new ArrayList<>();
             User user1 = new User(1L, "Machin", Fonction.USER);
             User user2 = new User(2L, "Chose", Fonction.USER);
             User user3 = new User(3L, "Truc", Fonction.DEVELOPPER);
-            List<User> users = new ArrayList<>(){{
-                add(user1);
-                add(user2);
-                add(user3);
-            }};
+
+            users.add(user1);
+            users.add(user2);
+            users.add(user3);
             userRepository.saveAll(users);
 
+            List<Issue> issues = new ArrayList<>();
             Issue issue1 = new Issue(1L, "blah", "some content1", user1, Timestamp.from(Instant.now()), null);
             Issue issue2 = new Issue(2L, "bleuh", "some content2", user2, Timestamp.from(Instant.now()), null);
             Issue issue3 = new Issue(3L, "blih", "some content3", user3, Timestamp.from(Instant.now()), null);
-            List<Issue> issues = new ArrayList<>(){{
-                add(issue1);
-                add(issue2);
-                add(issue3);
-            }};
+
+            issues.add(issue1);
+            issues.add(issue2);
+            issues.add(issue3);
             issueRepository.saveAll(issues);
-            List<Commentaire> commentaires = new ArrayList<>(){{
-                add(new Commentaire(1L, user1, issue1,"Contenu 1"));
-                add(new Commentaire(2L, user1, issue1,"Contenu 2"));
-                add(new Commentaire(3L, user2, issue1,"Contenu 3"));
-                add(new Commentaire(4L, user2, issue1,"Contenu 4"));
-                add(new Commentaire(5L, user2, issue2,"Contenu 5"));
-            }};
+
+            List<Commentaire> commentaires = new ArrayList<>();
+            Commentaire commentaire1 = new Commentaire(1L, user1, issue1, "Contenu 1");
+            Commentaire commentaire2 = new Commentaire(2L, user1, issue1, "Contenu 2");
+            Commentaire commentaire3 = new Commentaire(3L, user2, issue1, "Contenu 3");
+            Commentaire commentaire4 = new Commentaire(4L, user2, issue1, "Contenu 4");
+            Commentaire commentaire5 = new Commentaire(5L, user2, issue2, "Contenu 5");
+
+            commentaires.add(commentaire1);
+            commentaires.add(commentaire2);
+            commentaires.add(commentaire3);
+            commentaires.add(commentaire4);
+            commentaires.add(commentaire5);
             commentaireRepository.saveAll(commentaires);
         };
     }
-
 }
